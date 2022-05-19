599d3353f71247981c26c18811e8f70f74ef0e1a
Mr Jaguar <68953408+Itz-MrJ@users.noreply.github.com>
2022-05-19 16:53:23 +0000

    threading

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
-import socket               
+import socket 
+import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = '0.0.0.0'
-port = 5000            
-s.bind((host, port))       
-print('Listening...')
-s.listen(5)
+HEADER = 64
+PORT = 5001
+SERVER = ''
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
-c, addr = s.accept()       
-print ('Got connection from', addr)  
-while True:
-   c.send("Server please type: ")
-   print("From Client: ", c.recv(1024))
-c.close()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+
+    conn.close()
+        
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
