87a4a5bcd026717696aa75f1443089252ad83fb1
Mr Jaguar <68953408+Itz-MrJ@users.noreply.github.com>
2022-05-19 15:54:50 +0000

    public

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import socket               
 
 s = socket.socket()         # Create a socket object
-host = '0.0.0.0'    #private ip address of machine running fedora
+host = '180.151.116.131'    #private ip address of machine running fedora
 port = 5000                
 s.bind((host, port))       
-
+print('Listening...')
 s.listen(5)                
 c, addr = s.accept()       
-print 'Got connection from', addr    #this line never gets printed
+print ('Got connection from', addr)    #this line never gets printed
 while True:
    c.send(raw_input("Server please type: "))
-   print "From Client: ", c.recv(1024)
+   print("From Client: ", c.recv(1024))
 
 c.close()     
