2e202e51e79d475d2dcf478ccdad187d85ca3447
Mr Jaguar <68953408+Itz-MrJ@users.noreply.github.com>
2022-05-19 15:58:02 +0000

    publioc

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
+import socket               
 
-import socket
-
-
-def server_program():
-    # get the hostname
-    host = socket.gethostname()
-    print(host)
-    port = 5000  # initiate port no above 1024
-
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
-
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
-
-    conn.close()  # close the connection
-
-
-if __name__ == '__main__':
-    server_program()
+s = socket.socket()         # Create a socket object
+host = '180.151.116.131'    #private ip address of machine running fedora
+port = 5000                
+s.bind((host, port))       
+print('Listening...')
+s.listen(5)                
+c, addr = s.accept()       
+print ('Got connection from', addr)    #this line never gets printed
+while True:
+   c.send(raw_input("Server please type: "))
+   print("From Client: ", c.recv(1024))
+c.close()
