48c0463dc4ea5589917943f47bfee75e53575fe1
Mr Jaguar <68953408+Itz-MrJ@users.noreply.github.com>
2022-05-19 16:41:08 +0000

    socketss

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import socket               
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = '0.0.0.0'    
-port = 5000                
+host = '0.0.0.0'
+port = 5000            
 s.bind((host, port))       
 print('Listening...')
 s.listen(5)
@@ -10,6 +10,6 @@ s.listen(5)
 c, addr = s.accept()       
 print ('Got connection from', addr)  
 while True:
-   c.send(raw_input("Server please type: "))
+   c.send("Server please type: ")
    print("From Client: ", c.recv(1024))
 c.close()
