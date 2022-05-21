889acfa5face49eccb108e3760def970f389db91
Mr Jaguar <68953408+Itz-MrJ@users.noreply.github.com>
2022-05-21 13:31:48 +0000

    exact path

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ server.bind(ADDR)
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
     msg_length = conn.recv(HEADER).decode(FORMAT)
-    driver = webdriver.Chrome('./chromedriver')
+    driver = webdriver.Chrome('/home/pi/sockets/chromedriver')
     print(msg_length)
     print('fetching')
     test = driver.get("https://api.tracker.gg/api/v2/valorant/standard/profile/riot/mrj%2300003/")
